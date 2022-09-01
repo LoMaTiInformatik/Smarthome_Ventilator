a4bafc42315185e65627171b881413f8a974d647
Luigi Crafter <luigicrafter04@gmail.com>
2022-09-01 23:28:27 +0200

    Big improvement

--- test/test.ino
+++ test/test.ino
@@ -26,13 +26,13 @@ void setup() {
 }
 
 void loop() {
-    if(esp8266.find("+IPD,")) {
-        int timeout = 3000;
+    if(esp8266.find("+IPD,")) {       //look for request start
+        int timeout = 500;
         long int times = millis();
 
         String result = "";
 
-        while((times+timeout) > millis()) {
+        while((times+timeout) > millis()) {   //save whole request
             while(esp8266.available()) {
                 char c = esp8266.read();
 
@@ -42,16 +42,25 @@ void loop() {
 
         int connectionid = result.charAt(0) - 48;
 
-        int start = result.indexOf("/?");
+        int start = result.indexOf("/?act");
         int end = result.indexOf(" HTTP");
+        bool favi = (result.indexOf("favicon.ico") != -1);
+        bool isact = (result.indexOf("act=") != -1);          // check if request is valid
 
-        if(start != -1 && end != -1) {
+        Serial.println("Start:" + String(start));
+        Serial.println("End:" + String(end));
+
+        if(start != -1 && end != -1 && favi != true && isact == true) {
           String substr = result.substring((start + 2),end);
           Serial.println("Substr:" + substr);
-          sendWebpage(connectionid);
-          
+          sendResponse(200, connectionid);
+        } else if(favi == true) {
+          sendResponse(204, connectionid);
+        } else {
+          sendResponse(400, connectionid);
         }
 
+        delay(200);
         Serial.println(connectionid);
 
         String closeCommand = "AT+CIPCLOSE=";
@@ -59,38 +68,97 @@ void loop() {
         closeCommand+=connectionid;
         closeCommand+="\r\n";
 
-        sendData(closeCommand,1000,DEBUG);
+        sendData(closeCommand,20,DEBUG);
     }
 }
 
-void sendWebpage(int conid) {
-  /*String page = ("HTTP/1.1 200 OK\r\n"
-  "Content-Type: text/plain\r\n"
-  "Connection: Closed\r\n"
-  "\r\n"
-  "This is test!\r\n");*/
-  String page = "HTTP/1.1 204 no content\r\n";
+void sendResponse(int code, int conid) {    //Respond #1
+  String page = "";
+  if(code == 200) {
+    page = ("HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/plain\r\n"
+    "Connection: Closed\r\n"
+    "\r\n"
+    "This is test!\r\n");
+  } else if(code == 204) {
+    page = ("HTTP/1.1 204 No Content\r\n"
+    "Connection: Closed\r\n"
+    "\r\n");
+  } else if(code == 400) {
+    page = ("HTTP/1.1 400 Bad Request\r\n"
+    "Connection: Closed\r\n"
+    "Content-Type: text/plain\r\n"
+    "\r\n"
+    "Invalid request!\r\n");
+  } else if(code == 404) {
+    page = ("HTTP/1.1 404 Not Found\r\n"
+    "Connection: Closed\r\n"
+    "Content-Type: text/plain\r\n"
+    "\r\n"
+    "Not Found!\r\n");
+  }
 
-  int len = page.length() + 4;
+  int len = page.length();
 
   sendData("AT+CIPSEND=" + String(conid) + "," + String(len) + "\r\n", 100, DEBUG);
 
-  sendData(page, 3000, DEBUG);
+  delay(200);
+
+  esp8266.print(page);
 }
+
+/*
+void send200(int conid) {
+  String page = ("HTTP/1.1 200 OK\r\n"
+  "Content-Type: text/plain\r\n"
+  "Connection: Closed\r\n"
+  "\r\n"
+  "This is test!\r\n");
+
+  int len = page.length();
+
+  sendData("AT+CIPSEND=" + String(conid) + "," + String(len) + "\r\n", 100, DEBUG);
+
+  delay(200);
+
+  esp8266.print(page);
+}
+
+void send204(int conid) {
+  String page = ("HTTP/1.1 204 No Content\r\n"
+  "Connection: Closed\r\n"
+  "\r\n");
+
+  int len = page.length();
+
+  sendData("AT+CIPSEND=" + String(conid) + "," + String(len) + "\r\n", 100, DEBUG);
+
+  delay(200);
+
+  esp8266.print(page);
+}
+
+void send400(int conid) {
+  String page = ("HTTP/1.1 400 Bad Request\r\n"
+  "Connection: Closed\r\n"
+  "Content-Type: text/plain\r\n"
+  "\r\n"
+  "Invalid request!\r\n");
+
+  int len = page.length();
+
+  sendData("AT+CIPSEND=" + String(conid) + "," + String(len) + "\r\n", 100, DEBUG);
+
+  delay(200);
+
+  esp8266.print(page);
+}
+*/
 
 String sendData(String command, const int timeout, boolean debug) {
   String response = "";
 
-  if(command.substring(0, 4) == "HTTP") {
-    /*for(int i = 0; i == command.length(); i++) {
-      String c = command.charAt(i);
-      esp8266.print(c);
-      delay(10);
-    }*/
-    esp8266.print(command);
-  } else {
-    esp8266.print(command);
-  }
+  esp8266.print(command);
 
   long int times = millis();
 
